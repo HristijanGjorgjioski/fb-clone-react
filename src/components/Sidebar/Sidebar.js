ebf27a4c6449dcb628083d3ca734214a5ae29df2
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-28 13:14:10 +0200

    Done with sidbar component for now! Now is time to work on Feed component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Card, CardActionArea, CardMedia, Button, Typography, CardContent } from '@material-ui/core';
 
@@ -6,27 +6,35 @@ import useStyles from './styles';
 
 const Sidebar = () => {
     const classes = useStyles();
+    const [text, setText] = useState('Never give up on your dreams!');
+    const [image, setImage] = useState('');
 
     const onButtonClick = async () => {
-        const url = 'https://api.imgflip.com/get_memes';
+        const url = 'https://type.fit/api/quotes';
         const response = await axios.get(url);
-        const data = response.data.data.memes[0];
-    }
+        const data = response.data;
+        let randomNumber = Math.round(Math.random() * 1000);
+        setText(data[randomNumber]);
+        return setImage(`https://source.unsplash.com/random/${randomNumber}`);
+    }   
 
     return (
         <div className={classes.container}>
             <Card className={classes.card}>
                 <CardActionArea>
-                    <CardMedia component="img" alt="Fact Image" height="140" title="Fact Image" image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">Fact</Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                        Search for the keywords to learn more about each warning. To ignore, add // eslint-disable-next-line to the line before.
+                    <CardMedia component="img" alt="Fact Image" height="140" title="Fact Image" image={image} />
+                    <CardContent textAlign="center">
+                        <Typography gutterBottom variant="h5" component="h2">QUOTE</Typography>
+                        <Typography variant="subtitle1" component="p">
+                            {text.text}
+                        </Typography>
+                        <Typography color="textSecondary" component="p">
+                            {text.author}
                         </Typography>
                     </CardContent>
                 </CardActionArea>
+                <Button className={classes.button} variant="contained" color="primary" onClick={() => onButtonClick()}>GENERATE QUOTE</Button>
             </Card>
-            <Button className={classes.button} variant="contained" color="primary" onClick={onButtonClick}>NEXT FACT</Button>
         </div>
     )
 }
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -1,12 +1,19 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles (() => ({
+    container: {
+        borderRadius: '15px'
+    },
     card: {
-        borderRadius: '10px'
+        borderRadius: '10px',
+        justifyContent: 'center'
     },
     button: {
         borderRadius: '15px',
         marginTop: '5px',
-        marginLeft: '45px'
+        marginBottom: '5px',
+        alignItems: 'center',
+        alignContent: 'center',
+        justifyContent: 'center'
     }
 }))
