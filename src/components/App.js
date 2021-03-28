9311d7d104f4759b8d6b6af5b4a0297ae50a45d3
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-28 22:20:29 +0200

    Working on Register and Signin components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Auth from './Auth/Auth';
 import Home from './Home/Home';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Home />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/auth" component={Auth} />
+            </Switch>
         </BrowserRouter>
     )
 }
